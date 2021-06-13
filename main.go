b0b69d8331c6d069991f74276f785b7bec0f4815
Josh Willox <39120423+jcwillox@users.noreply.github.com>
2021-06-14 00:01:24 +1000

    Strip WSL environment path

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"nfetch/cmd"
+	"nfetch/pkg"
 	"os"
 )
 
 func main() {
+	pkg.StripWSLPath()
+
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
---
+++ pkg/utils.go
@@ -0,0 +1,23 @@
+package pkg
+
+import (
+	"os"
+	"strings"
+)
+
+// StripWSLPath removes all windows paths from the PATH on WSL as it includes many networked windows
+// directories which are very slow to access
+func StripWSLPath() {
+	_, isWSL := os.LookupEnv("WSL_DISTRO_NAME")
+	if !isWSL {
+		return
+	}
+	path := os.Getenv("PATH")
+	var newPath []string
+	for _, line := range strings.Split(path, string(os.PathListSeparator)) {
+		if !strings.HasPrefix(line, "/mnt/c") {
+			newPath = append(newPath, line)
+		}
+	}
+	os.Setenv("PATH", strings.Join(newPath, string(os.PathListSeparator)))
+}
