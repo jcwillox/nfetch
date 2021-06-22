f4df1e2441258a475519ba8dc6c7349cf9108735
Josh Willox <39120423+jcwillox@users.noreply.github.com>
2021-06-23 00:11:29 +1000

    Add Windows 11 logo

--- pkg/logo/logo_windows.go
+++ pkg/logo/logo_windows.go
@@ -3,19 +3,29 @@ package logo
 import (
 	_ "embed"
 	"github.com/spf13/viper"
+	"nfetch/pkg/sysinfo"
+	"strings"
 )
 
-//go:embed logos/windows.txt
-var windows string
+//go:embed logos/windows_10.txt
+var windows10 string
+
+//go:embed logos/windows_11.txt
+var windows11 string
 
 //go:embed logos/windows_legacy.txt
 var windowsLegacy string
 
 func GetLogo() (string, []int) {
 	logo := viper.GetString("logo")
-	if logo == "legacy" {
-		return windowsLegacy, []int{1, 2, 4, 3}
+	if logo == "" {
+		logo = sysinfo.Distro()
+	}
+	if strings.HasPrefix(logo, "Windows 10") {
+		return windows10, []int{4}
+	} else if strings.HasPrefix(logo, "Windows 11") {
+		return windows11, []int{12, 14, 4}
 	} else {
-		return windows, []int{4}
+		return windowsLegacy, []int{1, 2, 4, 3}
 	}
 }
